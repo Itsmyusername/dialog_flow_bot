8f879218bdf3ff0a2b4964bec3c1d3b763448562
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-02-01 00:57:56 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import logging
+import os
 
+
+from dotenv import load_dotenv
+from google.cloud import dialogflow
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import logging
 
 
-TOKEN="6815631711:AAH2day9hZ4a-inu9s0MxlAHV-bnVEybdps"
-# Enable logging
+load_dotenv()
+TOKEN=os.environ["TOKEN"]
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
@@ -14,8 +19,25 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(request={"session": session, "query_input": query_input})
+
+    return response.query_result.fulfillment_text
+
+def reply_to_user(update: Update, context: CallbackContext):
+    project_id = "newagent-rxdx"
+    session_id = str(update.message.chat_id)
+    text = update.message.text
+    language_code = "ru"
+
+    response_text = detect_intent_texts(project_id, session_id, text, language_code)
+    update.message.reply_text(response_text)
+
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -36,26 +58,12 @@ def echo(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Updater and pass it your bot's token.
     updater = Updater(TOKEN)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Start the Bot
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, reply_to_user))
     updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
