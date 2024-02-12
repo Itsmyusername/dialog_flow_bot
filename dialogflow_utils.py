a2d49c74c2822d8ed6c6a79b23f200d1413717c2
Itsmyusername <25662642+Itsmyusername@users.noreply.github.com>
2024-02-12 10:01:19 -0800

    Update dialogflow_utils.py

--- dialogflow_utils.py
+++ dialogflow_utils.py
@@ -1,7 +1,7 @@
 from google.cloud import dialogflow_v2 as dialogflow
 
 
-def send_to_dialogflow(project_id, session_id, text, language_code='ru-RU'):
+def send_in_dialogflow(project_id, session_id, text, language_code='ru-RU'):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
 
@@ -10,4 +10,4 @@ def send_to_dialogflow(project_id, session_id, text, language_code='ru-RU'):
     response = session_client.detect_intent(
         session=session, query_input=query_input
     )
-    return response.query_result.fulfillment_text
+    return response.query_result.fulfillment_tex
